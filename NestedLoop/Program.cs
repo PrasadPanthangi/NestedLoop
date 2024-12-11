a9b16026f92e4fce17ef1a1607bd95ff68091cbe
Prasad <prasad.panthangi@solvancia.in>
2024-12-11 11:38:51 +0530

    Add project files.

---
+++ NestedLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tables");
+int i = 1;
+while (i <= 5)
+{
+    int j = 1;
+    while (j <= 10)
+    {
+        Console.WriteLine(i + "*" + j + "=" + (i * j));
+        j++;
+    }
+    i++;
+}
+    
